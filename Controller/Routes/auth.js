e9430d5cf0f5a0f849188f249a7bce90b455f047
lalebdi <loai.alebdi@gmail.com>
2020-08-23 13:48:57 -0400

    setted up the routes for the server

---
+++ Controller/Routes/auth.js
@@ -0,0 +1,19 @@
+// setting up the routes for the server
+const route = require('express').Router();
+
+// the /user is the end point of the route
+route.post('/user', (req,res) => {
+    
+})
+//  put is for updates
+route.put('/user', (req,res) => {
+
+})
+// thats for logging in
+.post('/', (req,res) => {
+
+})
+
+.get('/', (req,res) => {
+
+})
